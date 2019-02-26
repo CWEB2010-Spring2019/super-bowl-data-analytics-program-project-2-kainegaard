f867c4c12fe53ad12c430218ad993d1ff13af4dc
Kai Negaard <negkaij@dunwoody.edu>
2019-02-26 10:00:18 -0600

    Super Bowl Data Analytics

--- Project_Two/Program.cs
+++ Project_Two/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
 
 namespace Project_Two
 {
@@ -9,8 +13,62 @@ namespace Project_Two
             /**Your application should allow the end user to pass end a file path for output 
             * or guide them through generating the file.
             **/
+            
+            string filePath = Directory.GetCurrentDirectory();
+            string stepBackOne = Directory.GetParent(filePath).ToString();
+            string stepBackTwo = Directory.GetParent(filePath).ToString();
+            string stepBackThree = Directory.GetParent(filePath).ToString();
+            Console.WriteLine(stepBackThree);
 
-           
+            String[] arrayOfValues;
+            //List<Program> listOfPlayers;
+
+            string adjustedFilePath = $@"{stepBackThree}\Super_Bowl_Project.csv";
+            Console.WriteLine(adjustedFilePath);
+
+            if(File.Exists(adjustedFilePath)) 
+            {
+
+            FileStream aFile = new FileStream(adjustedFilePath, FileMode.Open, FileAccess.Read);
+            StreamReader read = new StreamReader(aFile);
+
+            while(!read.EndOfStream) 
+            {
+                //Serialize the objects into the program
+                arrayOfValues = read.ReadLine().Split(',');
+                Console.WriteLine(arrayOfValues.Length);
+                //listOfPlayers.Add(new Program(arrayOfValues[0], arrayOfValues[1], Int32.Parse(arrayOfValues[2]), arrayOfValues[3]));
+            }
+
+            read.Close();
+            aFile.Close();
+
+            //Creating an html from the file
+            using (FileStream fs = new FileStream("test.htm", FileMode.Create)) 
+            {
+                using (StreamWriter w = new StreamWriter(fs, Encoding.UTF8)) 
+                {
+                    w.WriteLine(@"Super_Bowl_Project.csv");
+                }
+            }
+            try
+        {
+
+             //Delete the file if it exists.
+            {
+                File.Delete(filePath);
+            }
         }
+            catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+            }
+        }
+
     }
+
 }
+    
+
+
